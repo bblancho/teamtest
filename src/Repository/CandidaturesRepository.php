12ceb86469bc07c34576433657e466b1d38e0bf5
blanchard <blanchard.banyingela@gmail.com>
2025-02-11 13:30:38 +0100

    resolution bug modification compte free-lance

--- src/Repository/CandidaturesRepository.php
+++ src/Repository/CandidaturesRepository.php
@@ -16,20 +16,34 @@ class CandidaturesRepository extends ServiceEntityRepository
         parent::__construct($registry, Candidatures::class);
     }
 
-    //    /**
-    //     * @return Candidatures[] Returns an array of Candidatures objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+    * @return Candidatures[] Returns an array of Candidatures objects
+    */
+    public function findByUser($user): array
+    {
+        return $this->createQueryBuilder(alias: 'c')
+            ->andWhere('c.clients = :idClient')
+            ->setParameter('idClient', $user)
+            ->orderBy('c.id', 'DESC')
+            ->setMaxResults(50)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function userAsPostule($user, $offre): bool
+    {
+        return $this->createQueryBuilder(alias: 'c')
+            ->andWhere('c.clients = :idClient and c.offres = :idOffre')
+            ->setParameters([
+                'idClient' => $user,
+                'idOffre' => $offre,
+            ])
+            ->setMaxResults(50)
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+    }
 
     //    public function findOneBySomeField($value): ?Candidatures
     //    {
