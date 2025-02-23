80e09f523edd9d675c0bd8d3b2d8abf47d90bb85
blanchard <blanchard.banyingela@gmail.com>
2025-02-23 15:02:02 +0100

    Création de l'entité Skills

---
+++ src/Entity/Skills.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SkillsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: SkillsRepository::class)]
+class Skills
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $slug = null;
+
+    #[ORM\ManyToOne(inversedBy: 'skills')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?users $users = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getUsers(): ?users
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?users $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+
+    
+}
