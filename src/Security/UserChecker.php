6d4bf6d25174ba8617527bd6c21d990ca26e4b6d
Mamadou MARÉGA <hamshakour93@gmail.com>
2025-05-29 22:29:14 +0200

    Ajout des vues

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Users;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof Users) {
+            return;
+        }
+
+        if (!$user->getIsVerified()) {
+            throw new CustomUserMessageAccountStatusException('Veuillez confirmer votre e-mail avant de vous connecter.');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
+}
