0ea8dd0bb777665c99a5e553fa38fc59efb161f3
Mamadou MARÉGA <hamshakour93@gmail.com>
2025-06-01 22:04:22 +0200

    Fix bug

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -108,6 +108,7 @@ class AppFixtures extends Fixture
                 ->setContraintes($this->faker->text(150))
                 ->setProfil($this->faker->text(200))
                 ->setExperience(mt_rand(1, 7))
+                ->setRefMission('')
             ;
 
             $manager->persist($job);
