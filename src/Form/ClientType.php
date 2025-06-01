0b5d2baef4bd3ff05fdb5e15c46943b4b32f8b52
Mamadou MARÉGA <hamshakour93@gmail.com>
2025-06-01 22:08:23 +0200

    Standardisation de code

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -19,78 +19,100 @@ class ClientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, [
-                'required' => true,
-                'attr' => [
-                    'class' => 'form-control',
-                    'minLength' => '2',
-                    'maxLength' => '50',
-                ],
-                'label' => 'Nom / Prénom',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-            ])
-            ->add('adresse', TextType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-control',
-                    'minLength' => '2',
-                    'maxLength' => '50',
-                ],
-                'label' => 'Adresse',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-            ])
-            ->add('cp', TextType::class, [
-                'required' => true,
-                'attr' => [
-                    'class' => 'form-control',
-                    'minLength' => '5',
-                    'maxLength' => '5',
-                ],
-                'label' => 'Code postal',
-                'label_attr' => [
-                    'class' => 'form-label mt-4'
-                ],
-            ])
-            ->add('ville', TextType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-control',
-                    'minLength' => '2',
-                    'maxLength' => '50',
-                ],
-                'label' => 'Ville',
-                'label_attr' => [
-                    'class' => 'form-label mt-0'
-                ],
-            ])
-            ->add('phone', TextType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-control',
-                    'exactly'   => '10',
-                ],
-                'label' => 'Télèphone',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-            ])
-            ->add('tjm', MoneyType::class, [
-                'required' => false,
-                'attr' => [
-                    'class' => 'form-control',
-                    'max' => 2000,
-                    'type' => 'number'
-                ],
-                'currency' => 'EUR',
-                'label' => 'Taux journalier :',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-            ])
+            ->add(
+                'nom',
+                TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'minLength' => '2',
+                        'maxLength' => '50',
+                    ],
+                    'label' => 'Nom / Prénom',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ],
+                ]
+            )
+            ->add(
+                'adresse', TextType::class,
+                [
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'minLength' => '2',
+                        'maxLength' => '50',
+                    ],
+                    'label' => 'Adresse',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ],
+                ]
+            )
+            ->add(
+                'cp', TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'minLength' => '5',
+                        'maxLength' => '5',
+                    ],
+                    'label' => 'Code postal',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4'
+                    ],
+                ]
+            )
+            ->add(
+                'ville',
+                TextType::class,
+                [
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'minLength' => '2',
+                        'maxLength' => '50',
+                    ],
+                    'label' => 'Ville',
+                    'label_attr' => [
+                        'class' => 'form-label mt-0'
+                    ],
+                ]
+            )
+            ->add(
+                'phone',
+                TextType::class,
+                [
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'exactly'   => '10',
+                    ],
+                    'label' => 'Télèphone',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ],
+                ]
+            )
+            ->add(
+                'tjm',
+                MoneyType::class,
+                [
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'max' => 2000,
+                        'type' => 'number'
+                    ],
+                    'currency' => 'EUR',
+                    'label' => 'Taux journalier :',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ],
+                ]
+            )
             // ->add('siren', TextType::class, [
             //     'mapped' => false,
             //     'attr' => [
@@ -102,31 +124,43 @@ class ClientType extends AbstractType
             //         'class' => 'form-label'
             //     ]
             // ]) 
-            ->add('cvFile', FileType::class,[
-                'required'  => false,
-                'mapped'    => false,
-                'attr' => [
-                    'class' => 'form-control',
-                ],
-                'label' =>' Déposer votre cv ',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-            ])   
-            ->add('isNewsletter', CheckboxType::class, [
-                'required' => false,
-                'empty_data' => '',
-                'attr' => [
-                    'class' => 'form-check-input ',
-                ],
-                'label' => "S'inscrire à la newsletter ?",
-                'label_attr' => [
-                    'class' => 'form-check-label '
-                ],
-            ])     
+            ->add(
+                'cvFile',
+                FileType::class,
+                [
+                    'required'  => false,
+                    'mapped'    => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label' =>' Déposer votre cv ',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ],
+                ]
+            )
+            ->add(
+                'isNewsletter',
+                CheckboxType::class,
+                [
+                    'required' => false,
+                    'empty_data' => '',
+                    'attr' => [
+                        'class' => 'form-check-input ',
+                    ],
+                    'label' => "S'inscrire à la newsletter ?",
+                    'label_attr' => [
+                        'class' => 'form-check-label '
+                    ],
+                ]
+            )
         ;
     }
 
+    /**
+     * @param OptionsResolver $resolver
+     * @return void
+     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
