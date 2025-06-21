8db72653b009f5c99d86e3f2f9b9a18ae99c1ab7
Blanchard <blanchard.banyingela@gamil.com>
2025-06-21 16:52:48 +0000

    ajout des modif de Hamzah

---
+++ migrations/Version20250621160448.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250621160448 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE clients ADD cv_file VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE offres CHANGE profil profil LONGTEXT NOT NULL, CHANGE ref_mission ref_mission VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE users ADD confirmation_token VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('ALTER TABLE offres CHANGE profil profil LONGTEXT DEFAULT NULL, CHANGE ref_mission ref_mission VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE users DROP confirmation_token');
+        $this->addSql('ALTER TABLE clients DROP cv_file');
+    }
+}
