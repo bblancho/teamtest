d360a4ba6a1486efa0614aa265ef80f400d9310d
blanchard <blanchard.banyingela@gmail.com>
2025-08-24 13:58:21 +0200

    refacto entité user

---
+++ src/Form/SearchType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'query',
+                TextType::class,
+                [
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'minLength' => '2',
+                        'placeholder' => "Chercher une opportunité par mot clé...",
+                    ]
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            "method" => "GET",
+            'csrf_protection' => false
+        ]);
+    }
+}
